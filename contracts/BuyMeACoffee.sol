d8f171161e725f1e167dc9592870ef5060014d86
Jennifer Tieu <jennifer@yammm.com>
2022-10-26 21:21:26 -0500

    bulk upload

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // list of memos received from friends
+    Memo[] memos;
+
+    // Address of the contract deployer
+    address payable owner;
+    
+    // Deploy logic. (only runs once, when deployed)
+    constructor() {
+        // saves the address
+        owner = payable(msg.sender);
+    } 
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+
+    }
+
+}
